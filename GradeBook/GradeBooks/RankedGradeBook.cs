aa66102436e37a5d2c7b629817e2a1d00c5d26d6
Monika <m.szymanska-macik@gmail.com>
2020-11-22 21:41:49 +0100

    grades added

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -17,23 +18,23 @@ namespace GradeBook.GradeBooks
             {
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
             }
-            return 'F';
-            if (averageGrade > 80)
-            {
+
+            var best20Students = (int)Math.Ceiling(Students.Count * 0.2);
+            var grades = Students.OrderByDescending(e => e.AverageGrade).Select(e => e.AverageGrade).ToList();
+            if (grades[best20Students - 1] <= averageGrade)
                 return 'A';
-            }
-            else if (averageGrade > 60)
-            { 
-                return 'B'; 
-            }
-            else if (averageGrade > 40)
-            {
+            else if
+                (grades[best20Students * 2 - 1] <= averageGrade)
+                return 'B';
+            else if
+                (grades[best20Students * 3 - 1] <= averageGrade)
                 return 'C';
-            }
-            else if(averageGrade > 20)
-            {
+            else if
+                (grades[best20Students * 4 - 1] <= averageGrade)
                 return 'D';
-            }
+            else
+                return 'F';
+            return base.GetLetterGrade(averageGrade);
         }
     }
 }
