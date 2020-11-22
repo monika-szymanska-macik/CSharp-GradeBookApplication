4bf49ff19d13a8bfe4a127d62fccbfa83d926482
Monika <m.szymanska-macik@gmail.com>
2020-11-22 21:09:35 +0100

    graded added

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -13,11 +13,27 @@ namespace GradeBook.GradeBooks
 
         public override char GetLetterGrade(double averageGrade)
         {
-                if (Students.Count < 5)
-                {
-                    throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
-                }
-                return 'F';
+            if (Students.Count < 5)
+            {
+                throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
+            }
+            return 'F';
+            if (averageGrade > 80)
+            {
+                return 'A';
+            }
+            else if (averageGrade > 60)
+            { 
+                return 'B'; 
+            }
+            else if (averageGrade > 40)
+            {
+                return 'C';
+            }
+            else if(averageGrade > 20)
+            {
+                return 'D';
+            }
         }
     }
 }
