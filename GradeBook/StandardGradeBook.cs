a84bfe903f08afbbe3434931eb9c16b2b9fda976
Monika <m.szymanska-macik@gmail.com>
2020-11-22 20:04:52 +0100

    New Enum GradeBookType created Property Type added to BaseGradeBook StandardGradeBook class created Standard`gradeBook Type updated RankedGRadeBook class created RankedGradeBook's Type updated

---
+++ GradeBook/StandardGradeBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using GradeBook.Enums;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook: BaseGradeBook
+    {
+        public StandardGradeBook(string name): base(name)
+        {
+            Type = GradeBookType.Standard;
+
+        }
+    }
+
+}
